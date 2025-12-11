fbecc45c5cd29673092b7d51735e4b0e8947b778
willy djabelkhir <willy.djabelkhir@wedafr.onmicrosoft.com>
2025-12-11 09:55:46 +0100

    fix: userService

---
+++ P7CreateRestApi/Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P7CreateRestApi.Application.ViewModels
+{
+    public class UserViewModel
+
+    {
+        public int Id { get; set; }
+        [Required] public string Username { get; set; } = null!;
+        [Required] public string Password { get; set; } = null!;
+        [Required] public string Fullname { get; set; } = null!;
+        [Required] public string Role { get; set; } = null!;
+
+    }
+}
---
+++ P7CreateRestApi/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using P7CreateRestApi.Application.ViewModels;
+using P7CreateRestApi.Entities;
+
+namespace P7CreateRestApi.Services
+
+{
+    public interface IUserService
+    {
+        Task<User> SaveUser(UserViewModel vm);
+
+        //Task UpdateBidList(BidListViewModel vm);
+
+        //Task<BidList> GetBidId(int id);
+
+        Task DeleteBidList(int id);
+    }
+}
