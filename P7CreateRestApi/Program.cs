70ad2bd88db92ad2eda8ba19d8f255c4f6c3c671
willy djabelkhir <willy.djabelkhir@wedafr.onmicrosoft.com>
2025-12-17 14:45:48 +0100

    add DI services

--- P7CreateRestApi/Program.cs
+++ P7CreateRestApi/Program.cs
@@ -1,17 +1,35 @@
+using System.Text;
+using Dot.Net.WebApi.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Text;
-using Dot.Net.WebApi.Data;
+using P7CreateRestApi.Application.Mapping;
+using P7CreateRestApi.Data.Repositories;
 using P7CreateRestApi.Entities;
+using P7CreateRestApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DbContext
 builder.Services.AddDbContext<LocalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+//Services métier et repositories
+builder.Services.AddScoped<IBidListRepository, BidListRepository>();
+builder.Services.AddScoped<IBidListService, BidListService>();
+builder.Services.AddScoped<ICurvePointRepository, CurvePointRepository>();
+builder.Services.AddScoped<ICurvePointService, CurvePointService>();
+builder.Services.AddScoped<IRatingRepository, RatingRepository>();
+builder.Services.AddScoped<IRatingService, RatingService>();
+builder.Services.AddScoped<ITradeRepository, TradeRepository>();
+builder.Services.AddScoped<ITradeService, TradeService>();
+builder.Services.AddScoped<IRuleRepository, RuleRepository>();
+builder.Services.AddScoped<IRuleService, RuleService>();
 
 // Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
