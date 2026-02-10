63a589f12cf997acb52541bcb95847846acbbefa
willy djabelkhir <willy.djabelkhir@wedafr.onmicrosoft.com>
2026-02-10 14:36:43 +0100

    ignore logs and coverage files

--- P7CreateRestApi.Tests/obj/Debug/net6.0/P7CreateRestApi.Tests.AssemblyInfo.cs
+++ P7CreateRestApi.Tests/obj/Debug/net6.0/P7CreateRestApi.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P7CreateRestApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbcd8c305903967b9927ba1fed8c3d5004af3b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53841cd6ea13c9b6fecc77a2c7ba6b6b333d2d8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("P7CreateRestApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P7CreateRestApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
